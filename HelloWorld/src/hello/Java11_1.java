9fba021b55ca3ef6ed55f59499d1fc2459d467fb
Eric5510153 <2105105173@nkust.edu.tw>
2022-12-23 15:42:09 +0800

    2022_12_23

---
+++ HelloWorld/src/hello/Java11_1.java
@@ -0,0 +1,22 @@
+package hello;
+
+
+
+public class Java11_1 {
+
+	public static void main(String[] args) {
+		// TODO 自動產生的方法 Stub
+
+	int i,j,k;
+	i=5;
+	j=5;
+	k=i++;
+	k=++j;
+	System.out.println("i="+i+",j="+j);
+	System.out.println("k="+k+",i="+i);
+	System.out.println("k="+k+",j="+j);
+		
+		
+	}
+
+}
